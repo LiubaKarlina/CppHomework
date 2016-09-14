8b2c81b33076807382a186c7ce42ba8d1739b96e
LiubaKarlina <space_slona@mail.ru>
2016-09-14 22:57:59 +0300

    task1 with unresolved symbol

---
+++ lab1_unresolved/src/algorithm.c
@@ -0,0 +1,9 @@
+#include "../include/io.h"
+#include "../include/util.h"
+#include "../include/algorithm.h" 
+
+int doComputation(int first, float second) {
+  int c = max(3, 4);    
+  logDebug('d');
+  return c;
+}
---
+++ lab1_unresolved/src/main.c
@@ -0,0 +1,11 @@
+#include "../include/io.h"
+#include "../include/util.h"  
+#include "../include/algorithm.h" 
+
+void main( void ) {
+  int a = doComputation(5, 7.0);
+  int b = 5;
+  max(a, b);
+  logDebug('!');
+  п
+}
